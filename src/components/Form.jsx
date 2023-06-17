12c9ff9b2d77f8429d2514145c88e8552b1e358a
michelcub <orestelopez96@gmail.com>
2023-06-17 11:46:09 +0100

    newchange

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,19 +1,28 @@
+import useAppContext from "../Context/AppContext";
+
+
 const Form = () => {
+
+    const {store, actions} = useAppContext();
+    const{handleOnSubmit,handleGetUserInput} = actions;
+    const{userInput} = store;
+    console.log(userInput)
+    
     return(
         <section className="col-9 p-5">
             <h1 className="text-center">Add new contact</h1>
-            <form action="" className="p-3">
+            <form action="POST" className="p-3" onSubmit={handleOnSubmit}>
                 <label className="w-100 mt-3" htmlFor="fullName">Full Name</label>
-                <input className="form-control w-100" type="text" placeholder="Full Name" name="full_name" id="fullNameInput"/>
+                <input onChange={handleGetUserInput}value={userInput.fullname} className="form-control w-100" type="text" placeholder="Full Name" name="fullname" id="fullNameInput"/>
 
                 <label className="w-100 mt-3" htmlFor="emailInput">Email</label>
-                <input className="form-control w-100" type="text" placeholder="Enter email" name="email" id="emailInput"/>
+                <input onChange={handleGetUserInput} value={userInput.email} className="form-control w-100" type="text" placeholder="Enter email" name="email" id="emailInput"/>
 
                 <label className="w-100 mt-3" htmlFor="phoneInput">Phone</label>
-                <input className="form-control w-100" type="text" placeholder="Enter phone" name="phone" id="phoneInput"/>
+                <input onChange={handleGetUserInput} value={userInput.phone} className="form-control w-100" type="text" placeholder="Enter phone" name="phone" id="phoneInput"/>
 
                 <label className="w-100 mt-3" htmlFor="addressInput">Address</label>
-                <input className="form-control w-100" type="text" placeholder="Enter Address" name="address" id="addressInput"/>
+                <input onChange={handleGetUserInput} value={userInput.address} className="form-control w-100" type="text" placeholder="Enter Address" name="address" id="addressInput"/>
 
                 <button className="btn btn-primary w-100 mt-4" type="submit">save</button>
             </form>
--- src/view/homeView.jsx
+++ src/view/homeView.jsx
@@ -1,8 +1,12 @@
 import Navbar from "../components/Navbar";
 import ContactList from "../components/ContactList";
 import ListItem from "../components/ListItem";
-
+import useAppContext from "../Context/AppContext";
 const HomeView = () => {
+
+    const {store, actions} = useAppContext();
+    // console.log(store.newList)
+
     return(
             <section>
                 <Navbar/>
