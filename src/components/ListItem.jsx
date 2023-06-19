39513c24b73494921d201ee58ee117f70c9f2d88
lucas solari <solarilucas92@gmail.com>
2023-06-19 18:13:53 +0200

    estilos contact item

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,29 +1,31 @@
 import image from "./../assets/img/user-img.png";
-import "./../../styles.css"
+import styles from "./ListItem.module.css";
 
 const ListItem = ({id, name , address, phone, email}) => {
     return (
-        <li className="d-flex list-group-item " >
+        <div className="container-fluid">
+        <li className=" d-flex list-group-item " >
                 <div className="">
                 <img src={image} className="user-image rounded-circle object-fit-cover" alt="user-image" />
                 </div>
-                <div>
-                    <h2>{name}</h2>
-                    <ul>
-                        <li className="list-group-item"><a className="nav-link-item" href=""><i className="bi bi-geo-alt-fill"></i> {address}</a></li>
-                        <li className="list-group-item"><a className="nav-link-item" href=""><i className="bi bi-telephone-fill"></i>{phone}</a></li>
-                        <li className="list-group-item"><a className="nav-link-item" href=""><i className="bi bi-envelope-fill"></i> {email}</a></li>
+                <div className="ms-5">
+                    <h2>{name}Pedro Gutierrez</h2>
+                    <ul className="list-unstyled">
+                        <li><a className="nav-link-item text-decoration-none text-secondary" href=""><i className="bi bi-geo-alt-fill"></i> {address}</a></li>
+                        <li><a className="nav-link-item text-decoration-none text-secondary" href=""><i className="bi bi-telephone-fill"></i>{phone}</a></li>
+                        <li><a className="nav-link-item text-decoration-none text-secondary" href=""><i className="bi bi-envelope-fill"></i> {email}</a></li>
                     </ul>
                 </div>
-                <div>
-                    <button><i className="bi bi-pencil"></i>
+                <div className="ms-auto">
+                    <button className={styles.buttons}><i className="bi bi-pencil"></i>
                     </button>
-                    <button>
+                    <button className={styles.buttons}>
                         <i className="bi bi-trash-fill"></i>
                     </button>
                 </div>
             
         </li>
+        </div>
     )
 };
 
